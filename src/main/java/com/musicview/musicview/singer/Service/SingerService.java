b6d1eaf0022be09d7b8fdf2f41fa87ac65f29aed
sky <1063410652sky@gmail.com>
2018-07-18 10:24:49 +0800

    MOD: 添加歌曲地区分布统计

--- src/main/java/com/musicview/musicview/singer/Service/SingerService.java
+++ src/main/java/com/musicview/musicview/singer/Service/SingerService.java
@@ -21,6 +21,7 @@ import org.elasticsearch.search.aggregations.bucket.terms.Terms;
 import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
 import org.elasticsearch.search.aggregations.metrics.avg.AvgAggregationBuilder;
 import org.elasticsearch.search.aggregations.metrics.cardinality.Cardinality;
+import org.elasticsearch.search.aggregations.metrics.cardinality.CardinalityAggregationBuilder;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.query.QuerySearchRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,9 +40,10 @@ public class SingerService {
 
         Map<String,Object> stringObjectMap = new HashMap<String,Object>();
 
-
-        TermsAggregationBuilder genders = AggregationBuilders.terms("genders").field("singer.country.keyword");
+        CardinalityAggregationBuilder cardinality = AggregationBuilders.cardinality("result").field("singer.singer_id");
+        TermsAggregationBuilder genders = AggregationBuilders.terms("genders").subAggregation(cardinality).field("singer.country.keyword");
         SearchRequest request = new SearchRequest("mymusic").types("hotmusic3").source(new SearchSourceBuilder().aggregation(genders));
+
         System.out.println(request);
         SearchResponse response = client.search(request);
         Map<String, Aggregation> aggMap = response.getAggregations().asMap();
@@ -51,8 +53,14 @@ public class SingerService {
         while (iterator.hasNext()){
             Terms.Bucket next = iterator.next();
             String keyAsString = next.getKeyAsString();
+            Map<String, Aggregation> stringAggregationMap2 = next.getAggregations().asMap();
+            Cardinality cardinality1 = (Cardinality)stringAggregationMap2.get("result");
+            long value = cardinality1.getValue();
 
-            stringObjectMap.put(keyAsString.trim().equals("")?"未知":keyAsString,next.getDocCount());
+            Map<String,Object> stringObjectMap1 = new HashMap<String,Object>();
+            stringObjectMap1.put("singer_num",value);
+            stringObjectMap1.put("song_num",next.getDocCount());
+            stringObjectMap.put(keyAsString.trim().equals("")?"未知地区":keyAsString,stringObjectMap1);
         }
         System.out.println(stringObjectMap);
         return stringObjectMap;
